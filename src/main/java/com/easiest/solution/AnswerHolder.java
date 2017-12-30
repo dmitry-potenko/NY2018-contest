f207b1ff00570174d7097f4a35ba8d242c1aa9b4
dmitry.potenko <dmitry.potenko@moneyman.com>
2017-12-31 01:02:41 +0300

    149
    (cherry picked from commit e824d3a)

---
+++ src/main/java/com/easiest/solution/AnswerHolder.java
@@ -0,0 +1,9 @@
+package com.easiest.solution;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AnswerHolder {
+
+  public static AtomicLong javaEntranceCount = new AtomicLong();
+
+}
---
+++ src/main/java/com/easiest/solution/HereWeGo.java
@@ -0,0 +1,39 @@
+package com.easiest.solution;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HereWeGo {
+
+  private static final Logger logger = LoggerFactory.getLogger(HereWeGo.class);
+
+
+  public static void main(String[] args) throws Exception {
+    String crawlStorageFolder = System.getProperty("java.io.tmpdir");
+    int numberOfCrawlers = 7;
+
+    CrawlConfig config = new CrawlConfig();
+    config.setCrawlStorageFolder(crawlStorageFolder);
+
+    PageFetcher pageFetcher = new PageFetcher(config);
+    RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+    RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+    CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+    controller.addSeed("https://jprof.by/");
+
+    controller.start(JprofCrawler.class, numberOfCrawlers);
+
+    controller.waitUntilFinish();
+
+    logger.error("\n!!!--------------------------------\n\nHERE IS THE ANSWER: Java entrance count = " +
+        AnswerHolder.javaEntranceCount +
+        "\n\n--------------------------------!!!"
+    );
+  }
+}
